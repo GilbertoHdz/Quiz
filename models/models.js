35123cfc665e09e82eeb57d6e3f6cadc41cc43d5
GilbertoHdz <killer.krazydevil@gmail.com>
2015-07-16 00:00:18 -0500

    SQLite, Se agrega modelo y DB

---
+++ models/models.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+//Cargar ORM
+var Sequelize = require('sequelize');
+
+//User BBDD SQLite:
+var sequelize = new Sequelize(null. null, null,
+						{dialect: 'sqlite', storage: 'quiz.sqlite'}
+					);
+
+//Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+export.Quiz = Quiz; //Exportar la definición de la tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().success(function() {
+	//success(...) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function (count){
+		if(count === 0) { // la tabla se inicializa solo si esta vacia
+			Quiz.create({
+				pregunta:  'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function(){console.log('Base de datos Inicializada')});
+		};
+	});
+});
+
+
+
+
+
+
+
+
---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+//Definicion del modelo de Quiz
+
+module.exports = function(sequelize, DataType){
+	return sequelize.define('Quiz',
+			{
+				pregunta:  DataType.STRING,
+				respuesta: DataType.STRING,
+			});
+}
