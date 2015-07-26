66dca74dbc78e008ad880acea5b3a5b054a36865
GilbertoHdz <killer.krazydevil@gmail.com>
2015-07-26 18:00:45 -0500

    Se corrige Tema

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,20 +19,33 @@ exports.load = function (req, res, next, quizId) {
 
 // Get /quizes
 exports.index = function(req, res){
-	var searchName = req.query.search || "";
-	searchName = searchName.split(" ").join("%");
-	searchName = "%" + searchName + "%";
+	var searchName = req.query.search;
 
+	if (searchName) {
+		searchName = "%" + searchName + "%";
+		searchName = searchName.split(" ").join("%");	
 
-	models.Quiz.findAll({
-		where: ["lower(pregunta) like ?", searchName.toLowerCase()]	
-	}).then(
-		function (quizes){
-			res.render('quizes/index.ejs', {quizes: quizes, errors: [] });
-		}
-	).catch(function (error) {
-		next(error);
-	});
+		models.Quiz.findAll({
+			where: ["pregunta like ?", searchName],order:'pregunta ASC'
+		}).then(
+			function (quizes){
+				res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
+			}
+		).catch(function (error){ 
+			next(error); 
+		});
+
+	}else{
+		models.Quiz.findAll()
+		.then(
+			function (quizes){
+				res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
+			}
+		).catch( function (error){
+			next(error);
+		});
+	}
+
 };
 
 // Get /quizes/:id
@@ -61,19 +74,20 @@ exports.new = function(req, res) {
 
 // POST /quizes/create
 exports.create = function(req, res){
-	var quiz = models.Quiz.build( req.body.quiz );
+	var quiz = models.Quiz.build(req.body.quiz);
 
 	quiz
 	.validate()
 	.then(function (err) {
 		if (err) {
 			res.render('quizes/new', { quiz: quiz, errors: err.errors });
-		} else{
-			quiz // Save: guarda en DB campos pregunta y respuesta quiz
-			.save( {fields: ["pregunta", "respuesta", "tema"]} )
-			.then( function(){ res.redirec('/quizes')})
-		}; //res.redirect: Redireccion HTTP a la lista de preguntas
-	});
+		} else {
+			quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        	.save({fields: ["pregunta", "respuesta","tema"]})
+        	.then( function(){ res.redirect('/quizes')}) 
+            // res.redirect: Redirección HTTP a lista de preguntas
+		}
+	});  
 };
 
 
@@ -101,7 +115,7 @@ exports.update = function (req, res) {
 			.save( {fields: ["pregunta", "respuesta", "tema"]} )
 			.then( function() { res.redirect('/quizes'); });
 		};	// Redireccion HTTP a lista de preguntas (URL relativo)
-	});
+	}).catch(function(error){ next(error) });
 };
 
 
